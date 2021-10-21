df07b9c2925051e1c2bb4fb227319b6908ae5fca
Ani Azatyan <aniazatyan.2001@gmail.com>
2021-10-21 19:46:45 +0400

    Add project files.

---
+++ Schoolinfo/Program.cs
@@ -0,0 +1,66 @@
+﻿using SchoolInfo;
+using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var school = new SchoolManager("Hay Aspet", 2005, "Tonoyan");
+        Type myType = school.GetType();
+
+        Console.WriteLine("---------------Members-------------");
+        foreach (MemberInfo memberInfo in myType.GetMembers())
+        {
+            Console.WriteLine($"{memberInfo.MemberType} {memberInfo.Name}");
+        }
+
+        Console.WriteLine("---------------Methods-------------");
+        object[] array = new object[] { "Pushkin", 2000, "Sargsyan" };
+        MethodInfo[] methods = myType.GetMethods();
+        methods[0].Invoke(school, null);
+
+        MethodInfo[] methods2 = myType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+        methods2[0].Invoke(school, null);
+
+
+
+
+        foreach (MethodInfo method in myType.GetMethods())
+        {
+            Console.Write($"{method.ReturnType.Name} {method.Name} \n");
+            ParameterInfo[] parameters = method.GetParameters();
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                Console.Write($"{parameters[i].ParameterType.Name} {parameters[i].Name}");
+
+            }
+
+        }
+
+        Console.WriteLine("---------------Constructors-------------");
+        foreach (ConstructorInfo constructors in myType.GetConstructors())
+        {
+            Console.Write(myType.Name + " (");
+            ParameterInfo[] parameters = constructors.GetParameters();
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                Console.Write(parameters[i].ParameterType.Name + " " + parameters[i].Name);
+                if (i + 1 < parameters.Length)
+                    Console.Write(", ");
+            }
+            Console.WriteLine(")");
+        }
+        Console.WriteLine("---------------Fields-------------:");
+        foreach (FieldInfo field in myType.GetFields())
+        {
+            Console.WriteLine($"{field.FieldType} {field.Name}");
+        }
+
+        Console.WriteLine("---------------Properties-------------:");
+        foreach (PropertyInfo property in myType.GetProperties())
+        {
+            Console.WriteLine($"{property.PropertyType} {property.Name}");
+        }
+    }
+}
---
+++ Schoolinfo/SchoolManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SchoolInfo
+{
+    public class SchoolManager
+    {
+        private string[] students;
+        private string _name { get; set; }
+        private int _foundationYear { get; set; }
+        private string _directorName { get; set; }
+        public int _classNumber { get; set; }
+        public int _eachClassStudentNumber { get; set; }
+
+        public SchoolManager(string name, int year, string directorName)
+        {
+            _name = name;
+            _foundationYear = year;
+            _directorName = directorName;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"This is {_name} school,founded in {_foundationYear} year and the current director is {_directorName}");
+        }
+        private int NumberOfStudents(int eachClassNumber, int classNumber)
+        {
+            return eachClassNumber * classNumber;
+        }
+        public void Display(string[] students)
+        {
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
